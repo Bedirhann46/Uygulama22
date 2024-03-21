ed7c2a77a7c672c736f7b4a3c29a6c3db53f7ca1
bedirhann46 <bedirhannergiz46@gmail.com>
2024-03-21 12:11:14 +0300

    Proje dosyası ekle.

---
+++ Uygulama22/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace Uygulama22
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtKitapAdi = new System.Windows.Forms.TextBox();
+            this.txtKitapYazari = new System.Windows.Forms.TextBox();
+            this.txtKitapTuru = new System.Windows.Forms.TextBox();
+            this.btnKitapEkle = new System.Windows.Forms.Button();
+            this.ListeVeri = new System.Windows.Forms.ListBox();
+            this.btnListele = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Kitap Adı";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Kitap Yazarı";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 80);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Kitap Türü";
+            // 
+            // txtKitapAdi
+            // 
+            this.txtKitapAdi.Location = new System.Drawing.Point(95, 5);
+            this.txtKitapAdi.Name = "txtKitapAdi";
+            this.txtKitapAdi.Size = new System.Drawing.Size(100, 20);
+            this.txtKitapAdi.TabIndex = 3;
+            // 
+            // txtKitapYazari
+            // 
+            this.txtKitapYazari.Location = new System.Drawing.Point(95, 37);
+            this.txtKitapYazari.Name = "txtKitapYazari";
+            this.txtKitapYazari.Size = new System.Drawing.Size(100, 20);
+            this.txtKitapYazari.TabIndex = 4;
+            // 
+            // txtKitapTuru
+            // 
+            this.txtKitapTuru.Location = new System.Drawing.Point(95, 72);
+            this.txtKitapTuru.Name = "txtKitapTuru";
+            this.txtKitapTuru.Size = new System.Drawing.Size(100, 20);
+            this.txtKitapTuru.TabIndex = 5;
+            // 
+            // btnKitapEkle
+            // 
+            this.btnKitapEkle.Location = new System.Drawing.Point(120, 118);
+            this.btnKitapEkle.Name = "btnKitapEkle";
+            this.btnKitapEkle.Size = new System.Drawing.Size(75, 23);
+            this.btnKitapEkle.TabIndex = 6;
+            this.btnKitapEkle.Text = "Kitap Ekle";
+            this.btnKitapEkle.UseVisualStyleBackColor = true;
+            this.btnKitapEkle.Click += new System.EventHandler(this.btnKitapEkle_Click);
+            // 
+            // ListeVeri
+            // 
+            this.ListeVeri.FormattingEnabled = true;
+            this.ListeVeri.Location = new System.Drawing.Point(221, 5);
+            this.ListeVeri.Name = "ListeVeri";
+            this.ListeVeri.Size = new System.Drawing.Size(120, 134);
+            this.ListeVeri.TabIndex = 7;
+            // 
+            // btnListele
+            // 
+            this.btnListele.Location = new System.Drawing.Point(266, 161);
+            this.btnListele.Name = "btnListele";
+            this.btnListele.Size = new System.Drawing.Size(75, 23);
+            this.btnListele.TabIndex = 8;
+            this.btnListele.Text = "Listele";
+            this.btnListele.UseVisualStyleBackColor = true;
+            this.btnListele.Click += new System.EventHandler(this.btnListele_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnListele);
+            this.Controls.Add(this.ListeVeri);
+            this.Controls.Add(this.btnKitapEkle);
+            this.Controls.Add(this.txtKitapTuru);
+            this.Controls.Add(this.txtKitapYazari);
+            this.Controls.Add(this.txtKitapAdi);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtKitapAdi;
+        private System.Windows.Forms.TextBox txtKitapYazari;
+        private System.Windows.Forms.TextBox txtKitapTuru;
+        private System.Windows.Forms.Button btnKitapEkle;
+        private System.Windows.Forms.ListBox ListeVeri;
+        private System.Windows.Forms.Button btnListele;
+    }
+}
+
